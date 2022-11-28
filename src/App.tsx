737f701dbde075d48c63861817f62303dc4bd65b
Matheus Pimenta <matheuspimenta@Matheuss-Laptop.local>
2022-11-28 16:21:02 -0300

    form created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,52 @@
+import { useRef } from "react";
+
+
 function App() {
+
+  const searchForm = useRef<HTMLInputElement>(null);
+  
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const cep = searchForm.current?.value.replaceAll('-', '');
+    console.log(cep)
+  }
 
   return (
     <div className="App">
-      <h1 className='text-3xl font-bold underline'>Buscador de CEP</h1>
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Buscador de CEP
+            </h2>
+          </div>
+          <form className="mt-8 space-y-6">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <input
+                  id="email-address"
+                  name="cep"
+                  type="text"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Insira seu CEP"
+                  ref={searchForm} 
+                />
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                onClick={handleSubmit}
+              >
+                Buscar
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
